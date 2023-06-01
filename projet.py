61c442d80b9b9997105cd01db5807bd3d07a8780
Alexandre-github-code <alexandre.auguste@isen-ouest.yncrea.fr>
2023-06-01 10:41:08 +0200

    fin partie 1

--- projet.py
+++ projet.py
@@ -14,23 +14,39 @@ data = pd.read_csv('seismes_2014.csv', names = donnees, skiprows = 1)
 # Afficher les premières lignes du DataFrame
 # print(data)
 
-# afficher les lignes de data
+# Donner le nombre total de séismes enregistrés en 2014. 
 # print("nombre de séisme :", data.shape[0])
 
 # afficher le pays avec le plus de séisme 
 # print("pays avec le plus de séisme :", data["Pays"].value_counts().head(20))
 
-Pays = data["Pays"].value_counts().head(20).index.tolist()
-data_frame = []
+# Construire un code permettant d’obtenir la table des effectis des 20 lieux les plus fréquemment secoués dans le monde. 
+# noms = data["Pays"].value_counts().head(20).index.tolist()
 
-for lieu in Pays:
-    data_frame.append(data[data["Pays"] == lieu]["Magnitude"].tolist())
-    
+# Pour chacun de ces 20 lieux et pour la variable mag, représenter la boîte à moustaches.
+# magnitudes = []
 
-plt.boxplot(data_frame)
+# for lieu in noms:
+#     mag = data[data["Pays"] == lieu]["Magnitude"].tolist()
+#     magnitudes.append(mag)
 
-plt.ylim(0,10)
+# plt.xlabel("Pays")
+# plt.ylabel("Magnitude")
 
-pylab.xticks(Pays)
+# sns.boxplot(data=magnitudes, whis=[0,100])
+# plt.xticks(range(len(noms)), noms, rotation=90)
+# plt.show()
 
-plt.show()
+# Donner les 6 lieux du monde qui enregistrent les plus fortes magnitudes. 
+# max_mag = data.groupby("Pays")["Magnitude"].max().nlargest(6)
+# pays = data[data["Pays"].isin(max_mag.index)]["Pays"].unique()
+# print(pays)
+
+# Pour la Californie et l’Alaska, donner le nombre de séismes de magnitude inférieure ou égale à 2.
+# California = data[(data["Pays"] == "California") & (data["Magnitude"] <= 2)].shape[0]
+# Alaska = data[(data["Pays"] == "Alaska") & (data["Magnitude"] <= 2)].shape[0]
+
+# print("Nombre de micros-tremblement en Californie", California)
+# print("Nombre de micros-tremblement en Alaska", Alaska)
+
+
