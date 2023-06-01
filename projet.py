c86fd03c6fb87c7a37dfdb42a53d32e266c74236
Alexandre-github-code <alexandre.auguste@isen-ouest.yncrea.fr>
2023-06-01 10:01:36 +0200

    test boite à moutache

--- projet.py
+++ projet.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import pylab as pl
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 # Noms des donnes
 donnees = ["Instant", "Latitude", "Longitude", "Pays", "Magnitude", "Profondeur"]
@@ -13,4 +15,22 @@ data = pd.read_csv('seismes_2014.csv', names = donnees, skiprows = 1)
 # print(data)
 
 # afficher les lignes de data
-print(data.shape[0])
+# print("nombre de séisme :", data.shape[0])
+
+# afficher le pays avec le plus de séisme 
+# print("pays avec le plus de séisme :", data["Pays"].value_counts().head(20))
+
+Pays = data["Pays"].value_counts().head(20).index.tolist()
+data_frame = []
+
+for lieu in Pays:
+    data_frame.append(data[data["Pays"] == lieu]["Magnitude"].tolist())
+    
+
+plt.boxplot(data_frame)
+
+plt.ylim(0,10)
+
+pylab.xticks(Pays)
+
+plt.show()
